4a22c729fd2777b1a7b942e77ee7d0ed6c9492f7
ytremblais <yannick.tremblais@gmail.com>
2025-03-26 18:25:12 +0100

    Ajout du dossier APIGreenScoreDemo comme répertoire classique

---
+++ APIGreenScoreDemo/src/test/java/fr/apithinking/apigreenscore/demo/ApiGreenScoreDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.apithinking.apigreenscore.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGreenScoreDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
