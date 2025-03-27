89d5d18a62662903cae63ff8b0913367255bc722
ytremblais <yannick.tremblais@gmail.com>
2025-03-27 15:58:57 +0100

    fix: update error on tests classes

--- APIGreenScoreDemo/src/main/java/fr/apithinking/apigreenscore/demo/HelloController.java
+++ APIGreenScoreDemo/src/main/java/fr/apithinking/apigreenscore/demo/HelloController.java
@@ -1,13 +1,16 @@
 package fr.apithinking.apigreenscore.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class HelloController {
 
     @GetMapping("/hello")
     public String sayHello() {
+
         return "Hello, API Green Score!";
     }
 }
